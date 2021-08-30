be5671c641f0b2cdd757a7b574eda17f8d19575d
Lucas <contatooliveiralucas@gmail.com>
2021-08-30 14:47:53 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/calculator'
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+<React.StrictMode>
+  <Calculator />
+</React.StrictMode>
+ , document.getElementById('root'));
+
+reportWebVitals();
